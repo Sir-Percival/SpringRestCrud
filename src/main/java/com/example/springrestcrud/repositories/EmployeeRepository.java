ef8ecf216b080d4df437fb6bc1a983a576576df9
Kacper Walichnowski <kacperw112@wp.pl>
2024-02-13 18:52:51 +0100

    Use Spring Data REST

--- src/main/java/com/example/springrestcrud/repositories/EmployeeRepository.java
+++ src/main/java/com/example/springrestcrud/repositories/EmployeeRepository.java
@@ -2,6 +2,14 @@ package com.example.springrestcrud.repositories;
 
 import com.example.springrestcrud.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+//@RepositoryRestResource(path = "members")
+public interface EmployeeRepository extends JpaRepository<Employee, Integer>
+{
+    //http://localhost:8080/api/employees
+    //http://localhost:8080/api/employees?page=1
+    //http://localhost:8080/api/employees?sort=lastName
+    //http://localhost:8080/api/employees?sort=lastName,firstName,desc
+    //http://localhost:8080/api/employees?sort=lastName,firstName,desc&page=1
 }
